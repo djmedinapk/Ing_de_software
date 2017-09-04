b40abab8424f6cbedb2efaca63598d813eb8887c
david medina <djmedinapk@gmail.com>
2017-09-04 01:39:32 -0500

    Ejercicio-Tienda
    falta terminar  vista login -añadir al carrito - mostar carrito del logeado

---
+++ EjercicioTienda/App_Code/Datos/DAOtienda.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Npgsql;
+using NpgsqlTypes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+
+/// <summary>
+/// Descripción breve de DAOtienda
+/// </summary>
+public class DAOtienda
+{
+    public DAOtienda()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+    public DataTable consultar_login(Elogin sesion)
+    {
+        DataTable sesiones = new DataTable();
+
+        NpgsqlConnection conectar = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+
+        try
+        {
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("f_consultar_login", conectar);
+            dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;            
+            dataAdapter.SelectCommand.Parameters.Add("_username", NpgsqlDbType.Varchar).Value = sesion.Username;
+            dataAdapter.SelectCommand.Parameters.Add("_password", NpgsqlDbType.Varchar).Value = sesion.Password;
+
+            conectar.Open();
+            dataAdapter.Fill(sesiones);
+        }
+        catch (Exception Ex)
+        {
+            throw Ex;
+        }
+        finally
+        {
+            if (conectar != null)
+            {
+                conectar.Close();
+            }
+        }
+        return sesiones;
+    }
+
+    public DataTable listar_productos()
+    {
+        DataTable products = new DataTable();
+        NpgsqlConnection conectar = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+        try
+        {
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("f_obtener_productos",conectar);
+            dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            conectar.Open();
+            dataAdapter.Fill(products);
+        }
+        catch (Exception Ex)
+        {
+            throw Ex;
+        }
+        finally
+        {
+            if (conectar != null)
+            {
+                conectar.Close();
+            }
+        }
+        return products;
+    }
+
+    public DataTable listar_carrito(int? id)
+    {
+        DataTable carrito = new DataTable();
+        NpgsqlConnection conectar = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+        try
+        {
+            id = 1;
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("f_obtener_carrito", conectar);
+            dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            dataAdapter.SelectCommand.Parameters.Add("_iduser", NpgsqlDbType.Integer).Value = id;
+            conectar.Open();
+            dataAdapter.Fill(carrito);
+        }
+        catch (Exception Ex)
+        {
+
+            throw Ex;
+        }
+        finally
+        {
+            if (conectar != null)
+            {
+                conectar.Close();
+            }
+        }
+        return carrito;
+    }
+}
---
+++ EjercicioTienda/Controlller/Login.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Loginaspx : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+        Elogin login = new Elogin()
+        {
+            Username = Login1.UserName.ToString(),
+            Password = Login1.Password.ToString()
+        };
+        DAOtienda daoLogin = new DAOtienda();
+        DataTable informacion = daoLogin.consultar_login(login);
+        if (informacion.Rows.Count != 0)
+        {
+            Session["usuario"] = informacion;
+            Session["idusuario"] = informacion.Rows[0][0];
+            switch (informacion.Rows[0][5])
+            {
+                case 1:
+                    Response.Redirect("home.aspx");
+                    break;
+                case 2:
+                    Response.Redirect("home.aspx");
+                    break;                                             //Faltan 2 paginas para los roles!!
+                case 3:
+                    Response.Redirect("home.aspx");
+                    break;
+                case 4:
+                    Response.Redirect("home.aspx");
+                    break;
+                default:
+                    Response.Redirect("Login.aspx");
+                    break;
+
+            }
+        }
+        else
+        {
+            
+            //Lerror.ForeColor = System.Drawing.Color.Red;
+            //Lerror.Text = "Datos erróneos";
+        }
+    }
+}
