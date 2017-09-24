4c8015d72862a268f6703d3d2f3ffc6864d49455
Eduardo <l.eduard_26@hotmail.com>
2017-09-23 19:09:59 -0500

    Añadido ejercicio de archivos
    Falta mostrar los links de los archivos
    Carga de archivos en temp y a carpeta final listo
    Verificacion de cantidad de archivos totales y por tipo de archivos listos
    
    La carpeta Archivos_prueba contiene los archivos de carga para el ejercicio

---
+++ Ejercicio Archivos/Controller/Principal.aspx.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class View_Principal : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(Session["cantidad"] == null && Session["img"] == null && Session["pdf"] == null && Session["offi"] == null)
+        {
+            Session["cantidad"] = 0;
+            Session["img"] = 0;
+            Session["pdf"] = 0;
+            Session["offi"] = 0;
+        }
+        DataTable fotos;
+
+        if (Session["fotos"] == null)
+        {
+            fotos = new DataTable();
+            fotos.Columns.Add("ruta");
+            fotos.Columns.Add("descripcion");
+
+            Session["fotos"] = fotos;
+        }
+        else
+        {
+            fotos = (DataTable)Session["fotos"];
+        }
+
+        GridView1.DataSource = fotos;
+        GridView1.DataBind();
+
+    }
+    protected void B_Cargar_Click(object sender, EventArgs e)
+    {
+        Int32 cant, img, pdf, offi;
+        ClientScriptManager cm = this.ClientScript;
+        string nombreArchivo = System.IO.Path.GetFileName(FU_Cliente.PostedFile.FileName);
+        string extension = System.IO.Path.GetExtension(FU_Cliente.PostedFile.FileName);
+        
+        
+        //Insercion de archivo a carpeta temp
+        string saveLocation = Server.MapPath("~\\Archivos\\temp") + "\\" +"temp_" + nombreArchivo;
+        if (System.IO.File.Exists(saveLocation))
+        {
+            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Ya existe un archivo en el servidor con ese nombre');</script>");
+            return;
+        }
+        try
+        {
+            FU_Cliente.PostedFile.SaveAs(saveLocation);
+            //cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('El archivo temporal ha sido cargado');</script>");
+        }
+        catch (Exception exc)
+        {
+            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Error: ');</script>");
+            return;
+        }
+
+        //Evaluacion del tipo de archivo y su respectivo proceso de guardado de temp a carpeta final
+        switch (extension)
+        {
+            case ".jpg": case ".gif": case ".jpge": case ".png":
+                cant = int.Parse(Session["cantidad"].ToString());
+                if (cant < 10) //Igual nunca llega a superar 10 archivos por las otras verificaciones
+                {
+                    img = int.Parse(Session["img"].ToString());
+                    if (img < 3)
+                    {
+                        string saveLocation_final = Server.MapPath("~\\Archivos\\imagenes") + "\\" + "img_" + nombreArchivo;
+                        if (System.IO.File.Exists(saveLocation_final))
+                        {
+                            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Ya existe un archivo en el servidor con ese nombre');</script>");
+                            return;
+                        }
+                        try
+                        {
+                            System.IO.File.Move(saveLocation, saveLocation_final);
+                            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('El archivo ha sido cargado');</script>");
+                            //System.IO.File.Delete(saveLocation_final);
+                        }
+                        catch (Exception exc)
+                        {
+                            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Error: ');</script>");
+                            return;
+                        }
+                        cant++;
+                        Session["cantidad"] = cant;
+                       
+                        img++;
+                        Session["img"] = img;
+                    }
+                    else
+                    {
+                        cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('No puede ingresar mas imagenes');</script>");
+                        System.IO.File.Delete(saveLocation);
+                        return;
+                    }
+                }
+                else
+                {
+                    cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('No se pueden ingresar mas archivos');</script>");
+                    System.IO.File.Delete(saveLocation);
+                    return;
+                }
+                break;
+
+
+            case ".pdf":
+                cant = int.Parse(Session["cantidad"].ToString());
+                if (cant < 10)
+                {
+                    pdf = int.Parse(Session["pdf"].ToString());
+                    if (pdf < 4)
+                    {
+                        string saveLocation_final = Server.MapPath("~\\Archivos\\pdf") + "\\" + "pdf_" + nombreArchivo;
+                        if (System.IO.File.Exists(saveLocation_final))
+                        {
+                            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Ya existe un archivo en el servidor con ese nombre');</script>");
+                            return;
+                        }
+                        try
+                        {
+                            System.IO.File.Move(saveLocation, saveLocation_final);
+                            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('El archivo ha sido cargado');</script>");
+                            //System.IO.File.Delete(saveLocation_final);
+                        }
+                        catch (Exception exc)
+                        {
+                            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Error: ');</script>");
+                            return;
+                        }
+                        
+                        cant++;
+                        Session["cantidad"] = cant;
+                        
+                        pdf++;
+                        Session["pdf"] = pdf;
+                    }
+                    else
+                    {
+                        cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('No puede ingresar mas archivos de tipo pdf');</script>");
+                        System.IO.File.Delete(saveLocation);
+                        return;
+                    }
+                }
+                else
+                {
+                    cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('No se pueden ingresar mas archivos');</script>");
+                    System.IO.File.Delete(saveLocation);
+                    return;
+                }
+                break;
+            case ".docx":
+            case ".doc":
+            case ".xls":
+            case ".xlsx":
+            case ".pptx":
+            case ".ppt":
+            case ".txt":
+            case ".mdb":
+            case ".pps":
+            case ".pub":
+            case ".one":
+                cant = int.Parse(Session["cantidad"].ToString());
+                if (cant < 10)
+                {
+                    offi = int.Parse(Session["offi"].ToString());
+                    if (offi < 3)
+                    {
+                        string saveLocation_final = Server.MapPath("~\\Archivos\\ofimatica") + "\\" + "ofimatica_" + nombreArchivo;
+                        if (System.IO.File.Exists(saveLocation_final))
+                        {
+                            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Ya existe un archivo en el servidor con ese nombre');</script>");
+                            return;
+                        }
+                        try
+                        {
+                            System.IO.File.Move(saveLocation, saveLocation_final);
+                            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('El archivo ha sido cargado');</script>");
+                            //System.IO.File.Delete(saveLocation_final);
+                        }
+                        catch (Exception exc)
+                        {
+                            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Error: ');</script>");
+                            return;
+                        }
+                        
+                        cant++;
+                        Session["offi"] = cant;
+                        
+                        offi++;
+                        Session["offi"] = offi;
+                    }
+                    else
+                    {
+                        cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('No puede ingresar mas archivos de ofimatica');</script>");
+                        System.IO.File.Delete(saveLocation);
+                        return;
+                    }
+                }
+                else
+                {
+                    cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('No se pueden ingresar mas archivos');</script>");
+                    System.IO.File.Delete(saveLocation);
+                    return;
+                }
+                break;
+        }
+
+
+
+
+
+
+        /*DataTable fotos = new DataTable();
+
+        fotos = (DataTable)Session["fotos"];
+
+        string[] celdas = new string[2];
+
+        celdas[0] = "~\\Archivos\\Imagenes" + "\\" + "hola_" + nombreArchivo;
+        celdas[1] = nombreArchivo;
+
+
+        fotos.Rows.Add(celdas);
+
+        Session["fotos"] = fotos;
+
+        GridView1.DataSource = fotos;
+        GridView1.DataBind();*/
+    }
+}
---
+++ Ejercicio Archivos/View/Principal.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="~/Controller/Principal.aspx.cs" Inherits="View_Principal" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <div>
+    
+        <table class="auto-style1">
+            <tr>
+                <td>
+                    <asp:FileUpload ID="FU_Cliente" runat="server" />
+                </td>
+                <td>
+                    <asp:Button ID="B_Cargar" runat="server" OnClick="B_Cargar_Click" Text="Cargar" />
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                        <Columns>
+                            <asp:TemplateField HeaderText="Celulares 1">
+                            <ItemTemplate>
+                                <table cellspacing="0" cellpadding="10">
+                                    <tr>
+                                        <td>
+                                            <table border="1" style="border-color: #999999; border-style: solid; background-color: #f5f5f5;"
+                                                cellspacing="0" cellpadding="0">
+                                                <tr>
+                                                    <td style="text-align: center; font-size: 10pt; padding: 20px 30px 2px 30px; border-style: none;">
+                                                        <asp:Image ID="Image1" runat="server" ImageUrl='<%#Eval("[ruta]") %>' Width="200" />
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td style="text-align: center; font-size: 10pt; border-style: none; padding: 10px 20px 10px 20px;">
+                                                        <strong>
+                                                            <asp:Label ID="Label3" runat="server"><%# Eval("[descripcion]") %></asp:Label>
+                                                        </strong>
+                                                    </td>
+                                                </tr>                  
+                                                <tr>
+                                                    <td style="text-align: center; font-size: 10pt; padding: 0px 0px 5px 0px; border-style: none;">
+                                                        <asp:LinkButton ID="LinkButton1" runat="server" Text="[Eliminar]" Font-Underline="false"
+                                                            CommandName="Delete"></asp:LinkButton>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
